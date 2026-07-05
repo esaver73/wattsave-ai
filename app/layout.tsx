1b4fed3c358c94f4dd4e09998fa112868e409e75
HADYAN ALAJMI <hfbalajmi@gmail.com>
2026-07-05 04:46:46 +0300

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
 import NavbarWrapper from "./components/NavbarWrapper";
-
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,13 +15,14 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "WattSave AI",
   description: "AI Powered Energy Optimization",
   verification: {
     google: "Yoz5xc6NqnC6yKyaWELrjPWb3NqpFIPXM_kiCpKDJ-Q",
   },
 };
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -33,14 +34,25 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full bg-gray-100">
-       
-          <NavbarWrapper />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-EDB4B63WDX"
+          strategy="afterInteractive"
+        />
 
-          <main className="flex-1">
-            {children}
-          </main>
-          <Footer />
-       
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-EDB4B63WDX');
+          `}
+        </Script>
+
+        <NavbarWrapper />
+
+        <main className="flex-1">{children}</main>
+
+        <Footer />
       </body>
     </html>
   );
